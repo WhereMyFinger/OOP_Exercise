1272265491dc2a3567a92ffe7322d16a205f9e34
NguyenLe241299 <49112036+WhereMyFinger@users.noreply.github.com>
2022-06-17 10:01:41 +0700

    Add bai 3

---
+++ Main.java
@@ -0,0 +1,87 @@
+package exercise03;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void editStudentById() {
+		
+	}
+
+	public static void main(String[] args) {
+		System.out.println(
+				"1. Add student \n2. Edit student by Id "
+						+ "\n3. Delete student by Id \n4. Sort student by GPA "
+						+ "\n5. Sort student by name \n6. Show student \n0. Exit");
+
+		Scanner scanner = new Scanner(System.in);
+		ArrayList<Student> studentList = new ArrayList<Student>();
+
+		boolean cont = true;
+		do {
+			System.out.println("Choose 1 function: [0 - 6]");
+			int funcId;
+			funcId = scanner.nextInt();
+
+			switch (funcId) {
+			case 1:
+				Student student = new Student();
+				student.addStudent();
+				studentList.add(student);
+				break;		
+			case 2:
+				System.out.println("Input student Id to edit: ");
+				int idToEdit;
+				idToEdit = scanner.nextInt();
+			  
+				for(Student std : studentList) {
+					if(std.getId() == idToEdit) {
+						std.addStudent();
+					}
+				}
+				break;
+			case 3: 
+				System.out.println("Input student Id to delete: "); 
+				int idToDelete;
+				idToDelete = scanner.nextInt();
+				
+				for(Student std : studentList) {
+					if(std.getId() == idToDelete) {
+						studentList.remove(std);
+					}
+				}
+				break;
+			case 4:
+				//Sort student list by GPA in ascending order
+				Collections.sort(studentList, new Comparator<Student>() {
+					  @Override 
+					  public int compare(Student std0, Student std1) { 
+						  // TODO Auto-generated method stub 
+					  return (int) (std0.getGpa() - std1.getGpa()); }
+					  });
+				break;
+			case 5:
+				//Sort student list by name in ascending order
+				Collections.sort(studentList, new Comparator<Student>() {
+					  @Override 
+					  public int compare(Student std0, Student std1) { 
+						  // TODO Auto-generated method stub 
+						  return std0.getName().compareTo(std1.getName()); }
+					  });
+				break;
+			case 6:
+				for(Student std : studentList) {
+					std.showStudent();
+				}
+				break;
+			case 0:
+				System.out.println("Good bye!");
+				cont = false;
+				break;
+			}
+		} while (cont);
+	}
+}
---
+++ Student.java
@@ -0,0 +1,88 @@
+package exercise03;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Comparator;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Student {
+	
+	private int id;
+	private String name;
+	private int age;
+	private String address;
+	private float gpa;
+	
+	@Override
+	public String toString() {
+		return "Student: id = " + this.id + ", name = " + this.name + 
+				", age = " + this.age + ", address = " + this.address + ", GPA = " + this.gpa;
+	}
+	
+	Scanner scanner = new Scanner(System.in);
+
+	public void addStudent() {
+		Student student = new Student();
+		
+		System.out.println("Input student Id: ");
+		this.id = scanner.nextInt();
+		
+		System.out.println("Input student name: ");
+		this.name = scanner.next();
+		
+		System.out.println("Input student age: ");
+		this.age = scanner.nextInt();
+		
+		System.out.println("Input student address: ");
+		this.address = scanner.next();
+		
+		System.out.println("Input student GPA: ");
+		this.gpa = scanner.nextFloat();
+	}
+	
+	public void showStudent() {
+			System.out.println(toString());
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public float getGpa() {
+		return gpa;
+	}
+	
+	public void setGpa(float gpa) {
+		this.gpa = gpa;
+	}
+}
